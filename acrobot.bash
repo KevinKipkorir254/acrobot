a8a10b95152efde98acaecab951e169fc8d853ac
KevinKipkorir254 <kevo@Kipkorir>
2025-07-12 02:11:50 +0300

    initial commit

---
+++ acrobot.bash
@@ -0,0 +1,5 @@
+# .bashrc
+# Obtained at https://dev-portal.onshape.com/keys
+ONSHAPE_API=https://cad.onshape.com
+ONSHAPE_ACCESS_KEY=7vpmTb9SUxYS3IG85JiUpBcP
+ONSHAPE_SECRET_KEY=wcWiJsqDbrB4XH5f9lZL0Ys3KjYYnvbldSO5T2GowmtVkHAU
